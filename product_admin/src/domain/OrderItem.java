ce2f2fb6251c8da586630523e8e60694bc39874f
monvi967 <monvi967@mds-xa101s-p052.student.otago.ac.nz>
2015-07-14 14:48:07 +1200

    added OrderItem class with get and set and toString

---
+++ product_admin/src/domain/OrderItem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author monvi967
+ */
+public class OrderItem {
+    public Integer quantity_purchased;
+    public Double purchase_price;
+
+    public Integer getQuantity_purchased() {
+        return quantity_purchased;
+    }
+
+    public void setQuantity_purchased(Integer quantity_purchased) {
+        this.quantity_purchased = quantity_purchased;
+    }
+
+    public Double getPurchase_price() {
+        return purchase_price;
+    }
+
+    public void setPurchase_price(Double purchase_price) {
+        this.purchase_price = purchase_price;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" + "quantity_purchased=" + quantity_purchased + ", purchase_price=" + purchase_price + '}';
+    }
+    
+    
+    
+}
