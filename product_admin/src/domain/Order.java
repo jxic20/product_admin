bc75353b2ce6cb8d5bfb560b0342426cf8ffd39c
monvi967 <monvi967@mds-xa101s-p052.student.otago.ac.nz>
2015-07-14 14:40:45 +1200

    added Order class with get and set and toString

---
+++ product_admin/src/domain/Order.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author monvi967
+ */
+public class Order {
+    public Integer order_ID;
+    public Date order_date;
+
+    public Integer getOrder_ID() {
+        return order_ID;
+    }
+
+    public void setOrder_ID(Integer order_ID) {
+        this.order_ID = order_ID;
+    }
+
+    public Date getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Date order_date) {
+        this.order_date = order_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "order_ID=" + order_ID + ", order_date=" + order_date + '}';
+    }
+    
+    
+}
