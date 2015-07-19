5767f04164df977c74959cc89344013d3d32fffd
Jvic <Jvic@QuikComputer>
2015-07-19 23:23:52 +1200

    Added main menu

--- product_admin/src/Main.java
+++ product_admin/src/Main.java
@@ -1,3 +1,6 @@
+
+import gui.Main_menu;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,11 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Main_menu mainMenu = new Main_menu();
+        mainMenu.setLocationRelativeTo(null);
+        mainMenu.setVisible(true);
+        
+        
     }
     
 }
