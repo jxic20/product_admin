6bc5ae11418bb3c7b67dbd650cb5ee6b22e0858e
Jvic <Jvic@QuikComputer>
2015-08-07 14:02:52 +1200

    Mark fix

--- product_admin/src/dao/Products_list.java
+++ product_admin/src/dao/Products_list.java
@@ -16,59 +16,61 @@ import gui.Product_entry;
  * @author monvi967
  */
 public class Products_list{
-    private static Collection<Product>  productList = new TreeSet();
-    private static Collection<String> categoryList = new TreeSet();
+//    private static Collection<Product>  productList = new TreeSet();
+//    private static Collection<String> categoryList = new TreeSet();
     private static Map<Integer, Product> productIDList = new HashMap();
     private static Map<String, Set<Product>> productCatSet = new HashMap();
     
     
     /*adding products to Category Set*/
-    public void addToSet(Product pdt, Integer id){
+    private void addToSet(Product pdt){
         String cat = pdt.getCategory();
         Set<Product> newPSet = new HashSet();
         
+        // does category exist?
         if ((productCatSet.get(cat)) == null){
+            // nope
             newPSet.add(pdt);
             productCatSet.put(pdt.getCategory(), newPSet);
-            if(Product_entry.selcat != null || !pdt.getCategory().equals(Product_entry.selcat)){
-                productCatSet.remove(Product_entry.selcat);
-                System.out.println("list is null" + Product_entry.selcat + " " + productCatSet.keySet() );
-                Product_entry.selcat = null;
-                System.out.println("list is null" + Product_entry.selcat);
-            }
+//            if(Product_entry.selcat != null || !pdt.getCategory().equals(Product_entry.selcat)){
+//                productCatSet.remove(Product_entry.selcat);
+//                System.out.println("list is null" + Product_entry.selcat + " " + productCatSet.keySet() );
+//                Product_entry.selcat = null;
+//                System.out.println("list is null" + Product_entry.selcat);
+//            }
         } else {
             System.out.println("list not null" + Product_entry.selcat);
             
            
-            if((productCatSet.get(Product_entry.selcat)).contains(Product_entry.p) || (productCatSet.get(Product_entry.selcat)).size() == 1){
-                System.out.println("list not null" + Product_entry.selcat + " > " + productCatSet.get(Product_entry.selcat).size() + productCatSet.keySet());
-                (productCatSet.get(Product_entry.selcat)).clear();
-                Product_entry.p = new Product();
-                if(productCatSet.get(Product_entry.selcat).size() < 1){
-                    System.out.println("list not null rem" + Product_entry.selcat + "," + productCatSet.get(Product_entry.selcat).size() + " " + Product_entry.p + " "+ productCatSet.keySet());
-                    //productCatSet.remove(Product_entry.selcat);
-                    //categoryList.remove(Product_entry.selcat);
-                    //System.out.println("list not null rem" + " "+ productCatSet.keySet());
-                    Product_entry.selcat = null;
-                }
-            
-            }
+//            if((productCatSet.get(Product_entry.selcat)).contains(Product_entry.p) || (productCatSet.get(Product_entry.selcat)).size() == 1){
+//                System.out.println("list not null" + Product_entry.selcat + " > " + productCatSet.get(Product_entry.selcat).size() + productCatSet.keySet());
+//                (productCatSet.get(Product_entry.selcat)).clear();
+//                Product_entry.p = new Product();
+//                if(productCatSet.get(Product_entry.selcat).size() < 1){
+//                    System.out.println("list not null rem" + Product_entry.selcat + "," + productCatSet.get(Product_entry.selcat).size() + " " + Product_entry.p + " "+ productCatSet.keySet());
+//                    //productCatSet.remove(Product_entry.selcat);
+//                    //categoryList.remove(Product_entry.selcat);
+//                    //System.out.println("list not null rem" + " "+ productCatSet.keySet());
+//                    Product_entry.selcat = null;
+//                }
+//            
+//            }
             
 
-            (productCatSet.get(cat)).add(pdt);
+            productCatSet.get(cat).add(pdt);
             
         }
     }
     
-    public Set<String> getKS(){
+    private Set<String> getKS(){
         
         return productCatSet.keySet();
     }
     
-    public void deleteFromCatSeT(Product pdt){
+    private void deleteFromCatSeT(Product pdt){
         System.out.println("dell" + Product_entry.selcat);
         
-        
+//        getKS
         //productCatSet.remove(Product_entry.selcat);
         
         //(productCatSet.get(pdt.getCategory())).remove(pdt);
@@ -83,18 +85,18 @@ public class Products_list{
     }
     
     public void add(Product pdt){
-        productList.add(pdt);
-        addToSet(pdt, pdt.getProduct_ID());
-        categoryList.add(pdt.getCategory());
+//        productList.add(pdt);
+        addToSet(pdt);
+//        categoryList.add(pdt.getCategory());
         productIDList.put(pdt.getProduct_ID(), pdt);
         
     }
     
     public void delete(Product pdt){
-        productList.remove(pdt);
-        categoryList.remove(pdt.getCategory());
+//        productList.remove(pdt);
+//        categoryList.remove(pdt.getCategory());
         productIDList.remove(pdt.getProduct_ID());
-        //deleteFromCat(pdt);
+//        deleteFromCat(pdt);
         
     }
     
@@ -108,11 +110,11 @@ public class Products_list{
     
     
     public Collection<Product> get(){
-        return productList;
+        return productIDList.values();
     }
     
     public Collection<String> getCategories(){
-       return categoryList;
+       return getKS();
     }
     
     public Map<Integer, Product> getProduct_IDs(){
